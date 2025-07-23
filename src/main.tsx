5f2d974c45a90f87a87355d215c59973eef4eabd
Uche27 <uchennaogoma2004@gmail.com>
2025-07-23 00:00:44 +0000

    commit changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import SWHandler from 'path-to-your-sw-handler'; // Make sure to import SWHandler
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+
+root.render(<App />);
+
+// Call ready() after render
+SWHandler.client.ready();
